7b323d65a92dec0e565a7baf8857bce5fcbd901f
Nicolas Navarro <nicosanlorenzo98@hotmail.com>
2022-07-26 22:06:10 -0300

    avances

--- src/components/Checkout/Chekout.jsx
+++ src/components/Checkout/Chekout.jsx
@@ -1,24 +1,61 @@
-import React from 'react'
+import React, {useState} from 'react'
+
 import TextField from '@mui/material/TextField';
 import { Button, Container } from '@mui/material'
 
+const initialState = {
+    nombre:'',
+    apellido:'',
+    email:'',
+}
+
 const Chekout = () => {
+
+    const [values,setValues] = useState(initialState);
+
+    const handleOnChange = (e) =>{
+        const{value,name} = e.target;
+        setValues({...values, [name]: value})
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log(values);
+    }
+
     return (
-        <div>
-            <Container>
+            <>
                 <h1>Finalizar tu compra</h1>
-                <Container sx={{m:2}}>
-                    <TextField id="outlined-basic" label="Nombre" variant="outlined" />
-                </Container>
-                <Container sx={{m:2}}>
-                    <TextField id="outlined-basic" label="Apellido" variant="outlined" />
-                </Container>
-                <Container sx={{m:2}}>
-                    <TextField id="outlined-basic" label="Email" variant="outlined" />
-                </Container>
-                <Button variant="contained" color="success">Confirmar Compra</Button>
-            </Container>
-        </div>
+                <form onSubmit={onSubmit}>
+                    <TextField 
+                        id="outlined-basic"
+                        label="Nombre" 
+                        variant="outlined" 
+                        name='nombre'
+                        value={values.nombre}
+                        onChange={handleOnChange}
+                        />
+                    <TextField 
+                        id="outlined-basic"
+                        label="Apellido"
+                        variant="outlined" 
+                        name='apellido'
+                        value={values.apellido}
+                        onChange={handleOnChange}
+                        />
+                    <TextField 
+                        id="outlined-basic"
+                        label="Email"
+                        variant="outlined"
+                        name='email'
+                        value={values.email}
+                        onChange={handleOnChange}
+                        />
+                <button>
+                    Confirmar Compra
+                </button>
+                </form> 
+            </>
     )
 }
 
